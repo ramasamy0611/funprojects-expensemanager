bb8b84fe9d60e6790210858ad38c0b00b7ce3380
ramasamy molaiapan <ramasamy0611@gmail.com>
2020-04-02 23:08:08 +0530

    Liquibase works for User table

---
+++ src/main/java/com/ram/projects/expensemanager/conf/DataSourceConfig.java
@@ -0,0 +1,16 @@
+package com.ram.projects.expensemanager.conf;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class DataSourceConfig {
+/*    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource dataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }*/
+}
